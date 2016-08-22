c6eed14d2e1846417d8eca0d2e23b1bf5030f46a
Jure Podgoršek <jurij.podgorsek@dlabs.si>
2016-08-23 00:13:40 +0200

    Initial (too slow)

---
+++ p565.go
@@ -0,0 +1,99 @@
+package main
+
+import "fmt"
+
+// Get number of divisors (cache results)
+var numDivisors = make(map[uint64]*[]uint64)
+
+func remDupes(slice []uint64) *[]uint64 {
+	ex := make(map[uint64]bool)
+
+	result := []uint64{}
+	for i := range slice {
+		if !ex[slice[i]] {
+			ex[slice[i]] = true
+			result = append(result, slice[i])
+		}
+	}
+
+	return &result
+}
+
+func containsNum(slice []uint64, num uint64) bool {
+	for _, el := range slice {
+		if el == num {
+			return true
+		}
+	}
+	return false
+}
+
+func arrMap(arr []uint64, f func(uint64) uint64) *[]uint64 {
+	result := make([]uint64, len(arr))
+
+	for i, val := range arr {
+		result[i] = f(val)
+	}
+
+	return &result
+}
+
+func numDiv(num uint64) *[]uint64 {
+	if val, ok := numDivisors[num]; ok {
+		return val
+	}
+
+	if num == 0 || num == 1 {
+		numDivisors[num] = &[]uint64{1}
+		return numDivisors[num]
+	}
+
+	count := num / 2
+	for count > 1 && num%count != 0 {
+		count -= 1
+	}
+	factor := num / count
+
+	subDivs := numDiv(count)
+	mulDivisors := arrMap(*subDivs, func(num uint64) uint64 {
+		return num * factor
+	})
+	divisors := *remDupes(append(*subDivs, *mulDivisors...))
+
+	numDivisors[num] = &divisors
+	return numDivisors[num]
+}
+
+func arrSum(array []uint64) uint64 {
+	sum := uint64(0)
+
+	for i := 0; i < len(array); i++ {
+		sum += array[uint64(i)]
+	}
+
+	return sum
+}
+
+func omega(num uint64) uint64 {
+	return arrSum(*numDiv(num))
+}
+
+func omDiv(maxNum uint64, divider uint64) uint64 {
+	results := []uint64{}
+
+	for i := uint64(1); i <= maxNum; i++ {
+		if i%(maxNum/100) == 0 {
+			fmt.Println(i/(maxNum/100), "%")
+		}
+
+		if omega(i)%divider == 0 {
+			results = append(results, i)
+		}
+	}
+
+	return arrSum(results)
+}
+
+func main() {
+	fmt.Println(omDiv(1000000, 2017))
+}
---
+++ p565.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+# Get number of divisors (cache results)
+numDivisors = {}
+
+def numDiv(num):
+    if num in numDivisors:
+        print(numDivisors)
+        return numDivisors[num]
+
+    if num == 0 or num == 1:
+        numDivisors[num] = set([1])
+        return numDivisors[num]
+
+    count = int(num / 2)
+    while count > 1 and num % count != 0:
+        count -= 1
+
+    subDivs = numDiv(count)
+    factor = int(num / count)
+    divisors = set([x * factor for x in subDivs]).union(subDivs)
+
+    numDivisors[num] = divisors
+    return divisors
+
+def omega(num):
+    return sum(numDiv(num))
+
+def omDiv(maxNum, divider):
+    results = []
+
+    for i in range(1, maxNum + 1):
+        if (i == int(maxNum / 100)):
+            print(i / (maxNum / 100), "%")
+
+        if omega(i) % divider == 0:
+            results.append(i)
+
+    return sum(results)
+
+print(omDiv(10**6, 2017))
